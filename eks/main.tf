c4381c1957e0a3003ca58a80621fe24786069c05
ajay balaji <104267348+ajay-bj@users.noreply.github.com>
2024-10-28 18:05:19 +0530

    Add files via upload

---
+++ eks/main.tf
@@ -0,0 +1,56 @@
+#Vpc
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks_cluster_vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
+
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/private_elb"       = 1
+
+  }
+}
+
+#EKS
+
+module "eks" {
+  source                         = "terraform-aws-modules/eks/aws"
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.29"
+  cluster_endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size       = 2
+      max_size       = 2
+      desired_size   = 2
+      instance_types = var.instance_types
+    }
+  }
+  enable_cluster_creator_admin_permissions = true
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
